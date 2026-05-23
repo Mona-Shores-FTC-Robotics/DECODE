69f77330bcf63daca3df4bfe88947d8509870fd3
Dustin Shunta <dustinhans@gmail.com>
2026-05-22 21:42:07 -0400

    new auto attempt

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Autos/Commands/Trajectories/MichianaGate2.pp
@@ -0,0 +1,447 @@
+{
+  "startPoint": {
+    "x": 29.66881998277347,
+    "y": 127.56244616709732,
+    "heading": "linear",
+    "startDeg": 0,
+    "endDeg": 134
+  },
+  "lines": [
+    {
+      "name": "Preloads",
+      "endPoint": {
+        "x": 35.5714900947459,
+        "y": 109.9638242894057,
+        "heading": "linear",
+        "startDeg": 0,
+        "endDeg": 135.5,
+        "degrees": 134
+      },
+      "controlPoints": [],
+      "color": "#96559B",
+      "id": "line-k5pq2c85urn",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": true
+    },
+    {
+      "name": "Art1",
+      "endPoint": {
+        "x": 23.6343669250646,
+        "y": 95.98776916451337,
+        "heading": "linear",
+        "startDeg": 135.5,
+        "endDeg": 270,
+        "degrees": 270
+      },
+      "controlPoints": [],
+      "color": "#96559B",
+      "id": "line-imsjuvkoayf",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": true
+    },
+    {
+      "name": "OpenGate1",
+      "endPoint": {
+        "x": 15.7,
+        "y": 73.8,
+        "heading": "constant",
+        "startDeg": 270,
+        "endDeg": 270,
+        "degrees": 270
+      },
+      "controlPoints": [
+        {
+          "x": 25.422049956933677,
+          "y": 72.86856158484066
+        }
+      ],
+      "color": "#96559B",
+      "id": "line-zehv56xhb4q",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": true
+    },
+    {
+      "name": "Shoot1",
+      "endPoint": {
+        "x": 53,
+        "y": 87,
+        "heading": "linear",
+        "startDeg": 270,
+        "endDeg": 134,
+        "degrees": 134
+      },
+      "controlPoints": [],
+      "color": "#96559B",
+      "id": "line-m2ql985794",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": true
+    },
+    {
+      "name": "Art2Stage",
+      "endPoint": {
+        "x": 23.756244616709733,
+        "y": 70.99397071490094,
+        "heading": "linear",
+        "startDeg": 134,
+        "endDeg": 270,
+        "degrees": 270
+      },
+      "controlPoints": [
+        {
+          "x": 25.921619293712315,
+          "y": 84.11584840654606
+        }
+      ],
+      "color": "#96559B",
+      "id": "line-zp96t535moh",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": true
+    },
+    {
+      "id": "mphobbm0-0e6nic",
+      "endPoint": {
+        "x": 23.48600344530577,
+        "y": 60.09194659776053,
+        "heading": "constant",
+        "reverse": false,
+        "startDeg": 270,
+        "endDeg": 0,
+        "degrees": 270
+      },
+      "controlPoints": [],
+      "color": "#96559B",
+      "name": "Art2",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": true
+    },
+    {
+      "name": "OpenGate2",
+      "endPoint": {
+        "x": 15.7,
+        "y": 73.8,
+        "heading": "constant",
+        "startDeg": 270,
+        "endDeg": 270,
+        "degrees": 270
+      },
+      "controlPoints": [
+        {
+          "x": 22.70088285960379,
+          "y": 72.95695521102498
+        }
+      ],
+      "color": "#96559B",
+      "id": "line-rnrkn5cuox",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": true
+    },
+    {
+      "name": "Shoot2",
+      "endPoint": {
+        "x": 53,
+        "y": 87,
+        "heading": "linear",
+        "startDeg": 270,
+        "endDeg": 134,
+        "degrees": 134
+      },
+      "controlPoints": [],
+      "color": "#96559B",
+      "id": "line-xrg0ie66rx",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": true
+    },
+    {
+      "name": "GateSlant1",
+      "endPoint": {
+        "x": 12.5,
+        "y": 60,
+        "heading": "constant",
+        "startDeg": 134,
+        "endDeg": 150,
+        "degrees": 150
+      },
+      "controlPoints": [],
+      "color": "#96559B",
+      "id": "line-dm3jq2npvd4",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": true
+    },
+    {
+      "name": "Shoot3",
+      "endPoint": {
+        "x": 53,
+        "y": 87,
+        "heading": "linear",
+        "startDeg": 150,
+        "endDeg": 134,
+        "degrees": 134
+      },
+      "controlPoints": [],
+      "color": "#96559B",
+      "id": "line-3lpktio2iof",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": true
+    },
+    {
+      "name": "GateSlant2",
+      "endPoint": {
+        "x": 12.5,
+        "y": 60,
+        "heading": "constant",
+        "startDeg": 134,
+        "endDeg": 150,
+        "degrees": 150
+      },
+      "controlPoints": [],
+      "color": "#96559B",
+      "id": "line-9whokyvhml",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": true
+    },
+    {
+      "name": "Shoot4",
+      "endPoint": {
+        "x": 53.97502153316107,
+        "y": 87.7312661498708,
+        "heading": "linear",
+        "startDeg": 150,
+        "endDeg": 134,
+        "degrees": 134
+      },
+      "controlPoints": [],
+      "color": "#96559B",
+      "id": "line-ha6zs7mrjep",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": true
+    },
+    {
+      "name": "GateSlant3",
+      "endPoint": {
+        "x": 12.5,
+        "y": 60,
+        "heading": "constant",
+        "startDeg": 134,
+        "endDeg": 150,
+        "degrees": 150
+      },
+      "controlPoints": [],
+      "color": "#96559B",
+      "id": "line-o9t1d0ll8m",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": true
+    },
+    {
+      "name": "Shoot5Final",
+      "endPoint": {
+        "x": 65,
+        "y": 100,
+        "heading": "linear",
+        "startDeg": 150,
+        "endDeg": 145,
+        "degrees": 145
+      },
+      "controlPoints": [],
+      "color": "#96559B",
+      "id": "line-kv06lcjt13",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": true
+    },
+    {
+      "name": "ParkNearGate",
+      "endPoint": {
+        "x": 35,
+        "y": 70.4,
+        "heading": "linear",
+        "startDeg": 145,
+        "endDeg": 180,
+        "degrees": 180
+      },
+      "controlPoints": [],
+      "color": "#96559B",
+      "id": "line-kwm7ywqyd",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": "",
+      "locked": true
+    }
+  ],
+  "shapes": [],
+  "sequence": [
+    {
+      "kind": "path",
+      "lineId": "line-k5pq2c85urn"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-imsjuvkoayf"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-zehv56xhb4q"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-m2ql985794"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-zp96t535moh"
+    },
+    {
+      "kind": "path",
+      "lineId": "mphobbm0-0e6nic"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-rnrkn5cuox"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-xrg0ie66rx"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-dm3jq2npvd4"
+    },
+    {
+      "kind": "wait",
+      "id": "mphoiqf6-v1rfz2",
+      "name": "Wait",
+      "durationMs": 750,
+      "locked": false
+    },
+    {
+      "kind": "path",
+      "lineId": "line-3lpktio2iof"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-9whokyvhml"
+    },
+    {
+      "kind": "wait",
+      "id": "mphoj2yj-h25gff",
+      "name": "Wait",
+      "durationMs": 750,
+      "locked": false
+    },
+    {
+      "kind": "path",
+      "lineId": "line-ha6zs7mrjep"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-o9t1d0ll8m"
+    },
+    {
+      "kind": "wait",
+      "id": "mphoj8jx-yv3jqf",
+      "name": "Wait",
+      "durationMs": 750,
+      "locked": false
+    },
+    {
+      "kind": "path",
+      "lineId": "line-kv06lcjt13"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-kwm7ywqyd"
+    }
+  ],
+  "pathChains": [
+    {
+      "id": "chain-mpho288k-qqyig0",
+      "name": "Main Chain",
+      "color": "#96559B",
+      "lineIds": [
+        "line-k5pq2c85urn",
+        "line-imsjuvkoayf",
+        "line-zehv56xhb4q",
+        "line-m2ql985794",
+        "line-zp96t535moh",
+        "line-rnrkn5cuox",
+        "line-xrg0ie66rx",
+        "line-dm3jq2npvd4",
+        "line-3lpktio2iof",
+        "line-9whokyvhml",
+        "line-ha6zs7mrjep",
+        "line-o9t1d0ll8m",
+        "line-kv06lcjt13",
+        "line-kwm7ywqyd",
+        "mphobbm0-0e6nic"
+      ]
+    }
+  ],
+  "settings": {
+    "xVelocity": 75,
+    "yVelocity": 65,
+    "aVelocity": 3.141592653589793,
+    "kFriction": 0.1,
+    "rWidth": 16,
+    "rHeight": 16,
+    "safetyMargin": 1,
+    "maxVelocity": 40,
+    "maxAcceleration": 30,
+    "maxDeceleration": 30,
+    "fieldMap": "decode.webp",
+    "robotImage": "/robot.png",
+    "theme": "auto",
+    "showGhostPaths": false,
+    "showOnionLayers": false,
+    "onionLayerSpacing": 6,
+    "onionColor": "#dc2626",
+    "onionNextPointOnly": false,
+    "showHeadingArrow": false,
+    "headingArrowLength": 50,
+    "headingArrowColor": "#ffffff",
+    "headingArrowThickness": 2,
+    "pathOpacity": 1
+  },
+  "version": "1.2.1",
+  "timestamp": "2026-05-23T01:39:11.371Z"
+}
