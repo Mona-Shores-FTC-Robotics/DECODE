558c9f1148acaf61951e34742c540ef8e82eccf4
Dustin Shunta <dustinhans@gmail.com>
2026-05-28 09:23:46 -0400

    update paths

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Autos/Commands/Trajectories/MichianaSimple.pp
@@ -0,0 +1,341 @@
+{
+  "startPoint": {
+    "x": 26.5,
+    "y": 130,
+    "heading": "linear",
+    "startDeg": 0,
+    "endDeg": 0
+  },
+  "lines": [
+    {
+      "name": "PreloadLaunch",
+      "endPoint": {
+        "x": 36,
+        "y": 107,
+        "heading": "linear",
+        "startDeg": 0,
+        "endDeg": 134,
+        "degrees": 134
+      },
+      "controlPoints": [],
+      "color": "#999000",
+      "id": "line-1otnncsq8yu",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "name": "PickupSet1",
+      "endPoint": {
+        "x": 23,
+        "y": 83.8,
+        "heading": "constant",
+        "startDeg": 270,
+        "endDeg": 270,
+        "degrees": 270
+      },
+      "controlPoints": [
+        {
+          "x": 24,
+          "y": 113
+        }
+      ],
+      "color": "#9985B6",
+      "id": "line-32vk4y5x8ri",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "name": "Drain1",
+      "endPoint": {
+        "x": 15.5,
+        "y": 74,
+        "heading": "constant",
+        "startDeg": 270,
+        "endDeg": 270,
+        "degrees": 270
+      },
+      "controlPoints": [
+        {
+          "x": 25,
+          "y": 82
+        }
+      ],
+      "color": "#56BB88",
+      "id": "line-oxb7bhrxs7l",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "name": "LaunchSet1",
+      "endPoint": {
+        "x": 36,
+        "y": 107,
+        "heading": "linear",
+        "startDeg": 270,
+        "endDeg": 134,
+        "degrees": 134
+      },
+      "controlPoints": [
+        {
+          "x": 47,
+          "y": 90
+        }
+      ],
+      "color": "#BB8856",
+      "id": "line-odtsk56609m",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "name": "PickupSet2",
+      "endPoint": {
+        "x": 23,
+        "y": 61,
+        "heading": "constant",
+        "startDeg": 270,
+        "endDeg": 270,
+        "degrees": 270
+      },
+      "controlPoints": [
+        {
+          "x": 24,
+          "y": 100
+        }
+      ],
+      "color": "#CC5D68",
+      "id": "line-rzhzqlqu7i",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "name": "Drain2",
+      "endPoint": {
+        "x": 15.5,
+        "y": 73.9,
+        "heading": "constant",
+        "startDeg": 270,
+        "endDeg": 270,
+        "degrees": 270
+      },
+      "controlPoints": [
+        {
+          "x": 25,
+          "y": 70
+        }
+      ],
+      "color": "#68B6A8",
+      "id": "line-dtz6jficvyn",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "name": "LaunchSet2",
+      "endPoint": {
+        "x": 36,
+        "y": 107,
+        "heading": "linear",
+        "startDeg": 270,
+        "endDeg": 134,
+        "degrees": 134
+      },
+      "controlPoints": [
+        {
+          "x": 47,
+          "y": 90
+        }
+      ],
+      "color": "#A5CD98",
+      "id": "line-y1w11saelv9",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "name": "GateSlantOut",
+      "endPoint": {
+        "x": 13.5,
+        "y": 58.5,
+        "heading": "linear",
+        "startDeg": 134,
+        "endDeg": 152,
+        "degrees": 152
+      },
+      "controlPoints": [
+        {
+          "x": 40.46030871003307,
+          "y": 57.803197353914
+        }
+      ],
+      "color": "#B67CBA",
+      "id": "line-z3i8knzjwr",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "name": "GateSlantReturn",
+      "endPoint": {
+        "x": 36,
+        "y": 107,
+        "heading": "linear",
+        "startDeg": 152,
+        "endDeg": 134,
+        "degrees": 134
+      },
+      "controlPoints": [
+        {
+          "x": 39.68026460859978,
+          "y": 55.93109151047408
+        }
+      ],
+      "color": "#7A6DB7",
+      "id": "line-ar14vxxeg9",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "name": "Park",
+      "endPoint": {
+        "x": 35,
+        "y": 70.4,
+        "heading": "linear",
+        "startDeg": 134,
+        "endDeg": 180,
+        "degrees": 180
+      },
+      "controlPoints": [
+        {
+          "x": 41,
+          "y": 85
+        }
+      ],
+      "color": "#9CCB6B",
+      "id": "line-wf86nqow18",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    }
+  ],
+  "shapes": [],
+  "sequence": [
+    {
+      "kind": "path",
+      "lineId": "line-1otnncsq8yu"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-32vk4y5x8ri"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-oxb7bhrxs7l"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-odtsk56609m"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-rzhzqlqu7i"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-dtz6jficvyn"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-y1w11saelv9"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-z3i8knzjwr"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-ar14vxxeg9"
+    },
+    {
+      "kind": "path",
+      "lineId": "line-wf86nqow18"
+    }
+  ],
+  "pathChains": [
+    {
+      "id": "chain-mpjqqhvm-sti6kd",
+      "name": "Main Chain",
+      "color": "#686A9D",
+      "lineIds": [
+        "line-1otnncsq8yu",
+        "line-32vk4y5x8ri",
+        "line-oxb7bhrxs7l",
+        "line-odtsk56609m",
+        "line-rzhzqlqu7i",
+        "line-dtz6jficvyn",
+        "line-y1w11saelv9",
+        "line-z3i8knzjwr",
+        "line-ar14vxxeg9",
+        "line-wf86nqow18"
+      ]
+    }
+  ],
+  "continuousChains": [
+    {
+      "name": "Set1 PickupAndDrain",
+      "lineIds": [
+        "line-32vk4y5x8ri",
+        "line-oxb7bhrxs7l"
+      ]
+    },
+    {
+      "name": "Set2 PickupAndDrain",
+      "lineIds": [
+        "line-rzhzqlqu7i",
+        "line-dtz6jficvyn"
+      ]
+    }
+  ],
+  "settings": {
+    "xVelocity": 75,
+    "yVelocity": 65,
+    "aVelocity": 3.141592653589793,
+    "kFriction": 0.1,
+    "rWidth": 16,
+    "rHeight": 16,
+    "safetyMargin": 1,
+    "maxVelocity": 40,
+    "maxAcceleration": 30,
+    "maxDeceleration": 30,
+    "fieldMap": "decode.webp",
+    "robotImage": "/robot.png",
+    "theme": "auto",
+    "showGhostPaths": false,
+    "showOnionLayers": false,
+    "onionLayerSpacing": 6,
+    "onionColor": "#dc2626",
+    "onionNextPointOnly": false,
+    "showHeadingArrow": false,
+    "headingArrowLength": 50,
+    "headingArrowColor": "#ffffff",
+    "headingArrowThickness": 2,
+    "pathOpacity": 1
+  },
+  "version": "1.2.1",
+  "timestamp": "2026-05-24T12:23:14.914Z"
+}
